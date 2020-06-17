43c129ac885130dea5fb9f7f8680aa80ada91cc4
Saurabh <singh97saurabh19@gmail.com>
2020-06-17 06:05:53 +0530

    Intial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Snipet.wsgi --log-file -
---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+# to return token
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+app_name = 'account'
+
+urlpatterns = [
+    path('register/',views.registration,name = "register"),
+    path('login/',obtain_auth_token,name="login"),
+]
