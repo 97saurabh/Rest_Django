acf3edfd8eb221ef65add05e8970ad9597c5af43
Saurabh <singh97saurabh19@gmail.com>
2020-06-17 06:28:43 +0530

    Intial

--- Snipet/settings.py
+++ Snipet/settings.py
@@ -130,5 +130,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/example.com/static/"
